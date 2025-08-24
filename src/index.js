6ad9a197e219445e92a5a7036c69dcd7b56e53e4
arcamp0130 <arcmp0130@gmail.com>
2025-08-23 20:49:13 -0600

    Supporting routes, using './src/public/' as root dir

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import express from "express";
-import { fileURLToPath } from "url";
 import path from "path";
+import { fileURLToPath } from "url";
+import router from "./routes/index.routes.js";
 
 const app = express();
 const port = 3030;
 
-// Getting current filename and directory name
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.get("/", (_, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+app.use("/", router);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
