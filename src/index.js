ecd8431d8ae2a2bdd2195a7ca1b63e48758ae0ff
arcamp0130 <arcmp0130@gmail.com>
2025-08-24 16:19:09 -0600

    Properly serving static files to client

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+import { fileURLToPath } from "url";
 import express from "express";
 import router from "./routes/index.routes.js";
+import path from "path";
 
 const app = express();
 const port = 3030;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", router);
 
 app.listen(port, () => {
